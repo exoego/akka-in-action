290107c4136d72bd47f696b052eec42a9623bcf7
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2020-10-04 13:50:05 +0900

    Address ExtensionKey deprecation
    https://doc.akka.io/docs/akka/2.5/project/migration-guide-2.4.x-2.5.x.html#extensionkey-deprecation

--- chapter-persistence/src/main/scala/aia/persistence/Settings.scala
+++ chapter-persistence/src/main/scala/aia/persistence/Settings.scala
@@ -6,7 +6,13 @@ import akka.actor._
 
 import com.typesafe.config.Config
 
-object Settings extends ExtensionKey[Settings]
+object Settings extends ExtensionId[Settings] with ExtensionIdProvider {
+  override def createExtension(system: ExtendedActorSystem): Settings = new Settings(system)
+
+  override def lookup(): ExtensionId[_ <: Extension] = Settings
+
+  override def get(system: ActorSystem): Settings = super.get(system)
+}
 
 class Settings(config: Config) extends Extension {
   def this(system: ExtendedActorSystem) = this(system.settings.config)
